58eb84cc870ebbf834e15a69ef660c006f1bc143
peoray <peoray@yahoo.com>
2023-11-02 17:31:44 +0100

    Fix build error and astro-compress error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,17 @@
 import { defineConfig } from 'astro/config'
 import { remarkReadingTime } from './src/utils/remark-reading-time.mjs'
 
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind'
-
-// https://astro.build/config
 import robotsTxt from 'astro-robots-txt'
-
-// https://astro.build/config
 import sitemap from '@astrojs/sitemap'
-
-// https://astro.build/config
 import image from '@astrojs/image'
-
-// https://astro.build/config
 import mdx from '@astrojs/mdx'
-
-// https://astro.build/config
 import inspectUrls from '@jsdevtools/rehype-url-inspector'
-
-// https://astro.build/config
 import react from '@astrojs/react'
-
-// https://astro.build/config
 import compress from 'astro-compress'
-
-// https://astro.build/config
 import partytown from '@astrojs/partytown'
-
-// https://astro.build/config
 import prefetch from '@astrojs/prefetch'
 
-// https://astro.build/config
 export default defineConfig({
   site: 'https://peoray.dev',
   markdown: {
