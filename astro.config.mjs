37c9156c58193f14c11e421bbcc8b9a4f0867fc4
peoray <peoray@yahoo.com>
2025-04-22 02:09:48 +0100

    feat: add optional lastmod field to blog schema

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,54 +1,53 @@
-import { defineConfig, sharpImageService } from 'astro/config'
-import { remarkReadingTime } from './src/utils/remark-reading-time.mjs'
-import { remarkModifiedTime } from './src/utils/remark-modified-time.mjs'
-import sitemap from '@astrojs/sitemap'
-import mdx from '@astrojs/mdx'
-import inspectUrls from '@jsdevtools/rehype-url-inspector'
-import react from '@astrojs/react'
-import partytown from '@astrojs/partytown'
+import { defineConfig, sharpImageService } from 'astro/config';
+import { remarkReadingTime } from './src/utils/remark-reading-time.mjs';
+import sitemap from '@astrojs/sitemap';
+import mdx from '@astrojs/mdx';
+import inspectUrls from '@jsdevtools/rehype-url-inspector';
+import react from '@astrojs/react';
+import partytown from '@astrojs/partytown';
 
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()],
-  },
-  trailingSlash: 'never',
-  prefetch: true,
-  image: {
-    service: {
-      entrypoint: 'astro/assets/services/sharp',
-      config: {
-        limitInputPixels: false,
-      },
-    },
-  },
-  site: 'https://peoray.dev',
-  markdown: {
-    remarkPlugins: [remarkReadingTime, remarkModifiedTime],
-    rehypePlugins: [
-      [
-        inspectUrls,
-        {
-          selectors: ['a[href]'],
-          inspectEach(url) {
-            url.node.properties.target = '_blank'
-          },
-        },
-      ],
-    ],
-  },
-  integrations: [
-    sitemap(),
-    mdx(),
-    react(),
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-    // (await import('@playform/compress')).default(),
-  ],
-})
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	trailingSlash: 'never',
+	prefetch: true,
+	image: {
+		service: {
+			entrypoint: 'astro/assets/services/sharp',
+			config: {
+				limitInputPixels: false,
+			},
+		},
+	},
+	site: 'https://peoray.dev',
+	markdown: {
+		remarkPlugins: [remarkReadingTime],
+		rehypePlugins: [
+			[
+				inspectUrls,
+				{
+					selectors: ['a[href]'],
+					inspectEach(url) {
+						url.node.properties.target = '_blank';
+					},
+				},
+			],
+		],
+	},
+	integrations: [
+		sitemap(),
+		mdx(),
+		react(),
+		partytown({
+			// Adds dataLayer.push as a forwarding-event.
+			config: {
+				forward: ['dataLayer.push'],
+			},
+		}),
+		// (await import('@playform/compress')).default(),
+	],
+});
