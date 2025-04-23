5099bd6cd624b7638837a2be2f40211d026534a0
peoray <peoray@yahoo.com>
2025-04-23 17:55:46 +0100

    feat: Update copy of about copy and refactor project component

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,30 +1,30 @@
 export default [
   {
     title: 'Nuxt SaaS Kit',
-    website: 'https://nuxtsaaskit.dev',
+    website: 'https://nuxtsaaskit.com/?ref=peoray.dev',
     description: 'Boilerplate that helps developers build SaaS products by providing essential features out of the box.',
   },
   {
     title: 'Linkvoy',
-    demo: 'https://linkvoy.dev',
+    website: 'https://linkvoy.com/?ref=peoray.dev',
     description: 'Everything you need to plan weddings without the chaos for professionals.',
   },
-  {
-    title: 'tailwindcss-boilerplate',
-    source: 'https://github.com/peoray/tailwindcss-boilerplate',
-    description: '📦 Quick boilerplate to get you started with TailwindCSS ',
-  },
-  {
-    title: 'Astro Starter Blog',
-    demo: 'https://astro-starter-blog.netlify.app/',
-    source: 'https://github.com/peoray/astro-starter-blog/',
-    description:
-      'A simple, hackable & minimalistic starter for Astro that uses Markdown for content.',
-  },
-  {
-    title: 'Astro Portfolio Starter',
-    demo: 'https://astro-portfolio-starter.netlify.app/',
-    source: 'https://github.com/peoray/astro-portfolio-starter/',
-    description: ' A simple portfolio theme for Astro powered by Tailwind CSS.',
-  },
+  // {
+  //   title: 'tailwindcss-boilerplate',
+  //   source: 'https://github.com/peoray/tailwindcss-boilerplate',
+  //   description: '📦 Quick boilerplate to get you started with TailwindCSS ',
+  // },
+  // {
+  //   title: 'Astro Starter Blog',
+  //   demo: 'https://astro-starter-blog.netlify.app/',
+  //   source: 'https://github.com/peoray/astro-starter-blog/',
+  //   description:
+  //     'A simple, hackable & minimalistic starter for Astro that uses Markdown for content.',
+  // },
+  // {
+  //   title: 'Astro Portfolio Starter',
+  //   demo: 'https://astro-portfolio-starter.netlify.app/',
+  //   source: 'https://github.com/peoray/astro-portfolio-starter/',
+  //   description: ' A simple portfolio theme for Astro powered by Tailwind CSS.',
+  // },
 ]
